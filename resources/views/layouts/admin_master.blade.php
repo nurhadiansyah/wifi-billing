d3341b88a148c7587086176cb78f650d696c5969
adi asyah <asyahadi19@gmail.com>
2026-08-18 00:36:09 +0800

    Add favicon to admin_master layout

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -7,6 +7,8 @@
     
     <meta name="description" content="Sistem Manajemen Billing WiFi" />
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('assets1/images/favicon.png') }}?v=2" />
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
